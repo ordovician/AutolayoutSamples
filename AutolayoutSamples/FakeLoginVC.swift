e93db64dd05b7e6d70789cb41f649a037b20f17a
Erik Engheim <erik.engheim@mac.com>
2016-06-21 17:26:12 +0200

    Further testing with more complex layouts

---
+++ AutolayoutSamples/FakeLoginVC.swift
@@ -0,0 +1,13 @@
+//
+//  FakeLoginVC.swift
+//  AutolayoutSamples
+//
+//  Created by Erik Engheim on 21/06/16.
+//  Copyright © 2016 Translusion. All rights reserved.
+//
+
+import UIKit
+
+class FakeLoginVC: UIViewController {
+    
+}
