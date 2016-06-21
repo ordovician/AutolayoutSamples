b6b13c9f431af6b520d0a32195739509356c7a32
Erik Engheim <erik.engheim@mac.com>
2016-06-21 17:37:21 +0200

    Actions for lengthening and shortening text in complex layout

--- AutolayoutSamples/FakeLoginVC.swift
+++ AutolayoutSamples/FakeLoginVC.swift
@@ -9,5 +9,14 @@
 import UIKit
 
 class FakeLoginVC: UIViewController {
+    @IBOutlet var messageTextView: UITextView!
+    
+    @IBAction func shortenMessage(sender: AnyObject) {
+        messageTextView.text = "Not much"
+    }
+    
+    @IBAction func lengthenMessage(sender: AnyObject) {
+        messageTextView.text = "The quick brown fox jumps over the lazy dog"
+    }
     
 }
