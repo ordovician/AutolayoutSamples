d6a336a519a42b6f015b4388a9b387ebbd11251e
Erik Engheim <erik.engheim@mac.com>
2016-06-22 10:49:28 +0200

    Testing expaning UILabel layout

---
+++ AutolayoutSamples/FakeCustomerInfoVC.swift
@@ -0,0 +1,22 @@
+//
+//  FakeCustomerInfoVC.swift
+//  AutolayoutSamples
+//
+//  Created by Erik Engheim on 22/06/16.
+//  Copyright © 2016 Translusion. All rights reserved.
+//
+
+import UIKit
+
+class FakeCustomerInfoVC: UIViewController {
+    @IBOutlet var messageLabel: UILabel!
+    
+    @IBAction func shortenMessage(sender: AnyObject) {
+        messageLabel.text = "Not much"
+    }
+    
+    @IBAction func lengthenMessage(sender: AnyObject) {
+        messageLabel.text = "The quick brown fox jumps over the lazy dog"
+    }
+    
+}
